d68da65b8babfcdf3c0b867858d4ce108adf8fde
清水ハイネ <heine.shimizu@gmail.com>
2019-11-17 01:38:56 +0900

    モック追加。不要なファイルを削除

---
+++ memo-react/src/components/Form.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Form = () => {
+  return (
+    <div>
+      <form>
+        <textarea
+          placeholder="memo"
+        ></textarea>
+        <button>submit</button>
+      </form>
+    </div>
+  )
+};
+
+export default Form;
---
+++ memo-react/src/components/Memo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledMemo = styled.div`
+  width: 20vw;
+  height: 20vw;
+  padding: 1em;
+  background: #e1f0fa;
+  overflow-x: auto;
+`;
+
+const Memo = () => {
+  return (
+    <StyledMemo>めも</StyledMemo>
+  )
+};
+
+export default Memo;
