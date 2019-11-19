c2fd7042a632769fbf30b8396df37d6d3e794e5f
清水園子 <s-shimizu@tsumikiinc.com>
2019-11-19 12:37:37 +0900

    データをローカルストレージに設定するよう実装を追加

--- memo-react/src/App.js
+++ memo-react/src/App.js
@@ -6,10 +6,21 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      todo: [],
+      todo: JSON.parse(localStorage.getItem("todo")) || []
     };
     this.handleAdd = this.handleAdd.bind(this);
     this.handleDel = this.handleDel.bind(this);
+  }
+
+  // todoList itemの追加
+  addTodo = () => {
+    // localStorageにtodoList stateを保存
+    localStorage.setItem("todo", JSON.stringify(this.state.todo))
+  }
+
+  // todoListからitemを削除
+  removeTodo = (memo) => {
+    localStorage.setItem("todo", JSON.stringify(this.state.todo.filter(x => x !== memo)));
   }
 
   handleAdd(e) {
@@ -23,6 +34,7 @@ class App extends React.Component {
       this.setState({todo: this.state.todo});
       e.target.memo.value = "";
       e.target.priority.checked = false;
+      this.addTodo();
     }
   }
 
@@ -30,6 +42,7 @@ class App extends React.Component {
     this.setState({
       todo: this.state.todo.filter(x => x !== memo)
     })
+    this.removeTodo(memo);
   }
 
   render() {
