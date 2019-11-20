234b7c2495acc49855fadd21fad13c86adf00865
清水園子 <s-shimizu@tsumikiinc.com>
2019-11-20 17:18:05 +0900

    Create Buttonスタイル調整

--- memo-react/src/components/_const/COLOR.js
+++ memo-react/src/components/_const/COLOR.js
@@ -1,4 +1,6 @@
 export default {
   CAUTIONBG: '#fae1e3',
   CAUTION: '#ff5764',
+  CREATEBG: '#e3fae1',
+  CREATE: '#78e673',
 }
--- memo-react/src/components/molecules/CreateButton.js
+++ memo-react/src/components/molecules/CreateButton.js
@@ -2,14 +2,19 @@ import React from 'react';
 import styled from 'styled-components';
 import IconCreate from '../atoms/IconCreate';
 import FlexBox from '../atoms/FlexBox';
+import COLOR from '../_const/COLOR';
 
 const StyledFlexBox = styled(FlexBox)`
   display: inline-flex;
   padding: 1em;
+  margin: 0.5em 1em;
   cursor: pointer;
   border-radius: 0.3em;
+  border: 1px solid ${COLOR.CREATE};
+  transition: background .2s, border .2s;
   &:hover {
-    background: #CCC;
+    background: ${COLOR.CREATEBG};
+    border: 1px solid transparent;
   }
 `;
 
