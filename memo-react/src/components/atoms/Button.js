7977f726f94721da393e6dc5fda2352caab349a8
清水園子 <s-shimizu@tsumikiinc.com>
2019-11-21 21:08:05 +0900

    ボタンの色を変更できるように実装を変更

--- memo-react/src/components/atoms/Button.js
+++ memo-react/src/components/atoms/Button.js
@@ -3,10 +3,11 @@ import styled from 'styled-components';
 const Button = styled.button`
   width: 100%;
   padding: 0.5em;
-  background: #CCC;
-  border: none;
+  background: ${props => props.color};
+  border: ${props => props.border ? props.border : 'transparent'} 1px solid;
   cursor: pointer;
   outline: none;
+  box-sizing: border-box;
 `;
 
 export default Button;
